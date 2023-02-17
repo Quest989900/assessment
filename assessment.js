4e8006cf7239bb02e6bef6179b343c928e5c8ecc
quest989900 <quest98.99.00@gmail.com>
2023-02-17 14:31:13 +0100

    initial assessment

---
+++ assessment.js
@@ -0,0 +1,27 @@
+/*
+INSTRUCTION: Branch out to a new branch called feat/your-name. Solve the following
+1. Write a query that prints a list of employee names and their salary
+(i.e.: the name attribute) from the Employee table in alphabetical order.
+
+The Employee table containing employee data 
+for a company is described as follows:
+
+
+
+where employee_id is an employee's ID number, 
+name is their name, 
+months is the total number of months they've been working for the company, 
+and salary is their monthly salary.
+
+
+2. https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+Query the Name of any student in STUDENTS 
+who scored higher than  Marks. Order your output 
+by the last three characters of each name. 
+If two or more students both have names ending in the 
+same last three characters (i.e.: Bobby, Robby, etc.), 
+secondary sort them by ascending ID.
+
+
+*/
